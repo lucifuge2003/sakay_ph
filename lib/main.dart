91b0f1d98538c83936d19261c7fe751a20239de4
Luiz Angelo Ropero <luizangelo.ropero@gmail.com>
2025-08-18 01:24:38 +0800

    feat: created data structures

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Sakay PH',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Text(
+        'Routes List Page Placeholder',
+      ), // temp placeholder for the actual home widget
+    );
   }
 }
