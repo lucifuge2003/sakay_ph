105e4032dcc969b44760d281ddb535cb8da2ecab
Luiz Angelo Ropero <luizangelo.ropero@gmail.com>
2025-08-18 01:38:36 +0800

    fix: added key in constructor

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
