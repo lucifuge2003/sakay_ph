2a8aa0893581c18f8f977e68a7d10f5e6f7a2760
Luiz Angelo Ropero <luizangelo.ropero@gmail.com>
2025-08-18 01:01:09 +0800

    fix: varname

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // title: 'Flutter Demo',
-      // theme: ThemeData(
-      //   primarySwatch: Colors.blue,
-      // ),
-      // home: ,
-    );
+    return MaterialApp();
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:your_app_name/main.dart';
+import 'package:sakay_ph/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
