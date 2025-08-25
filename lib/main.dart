3cf342ce6aa0e7fe3d8208e3dbfe970e77439506
Francoise Gurango <francoisegurango@Francoises-MacBook-Air.local>
2025-08-25 22:22:25 +0800

    feat(ui): implement search bar and background color

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Sakay PH', 
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Text(
-        'Hello World',
+      home: Scaffold(
+        backgroundColor: const Color(0xFFC8AD7E),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFFC8AD7E),
+          title: const TextField(
+            decoration: InputDecoration(
+              hintText: 'Search...',
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black, width: 1.0),
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              ),
+              hintStyle: TextStyle(color: Colors.white70),
+            ),
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Container(
+          color: const Color(0xFFC8AD7E), //background color
+          child: const Text(
+            'Hello World',
         // Ropero, Luiz Angelo T.
         // Arcuino, Shan Harvey
         // Gurango, Christine Francoise
         // Pamintuan, Alexia John
+          ),
+      ),
       ), // temp placeholder for the actual home widget
     );
   }
