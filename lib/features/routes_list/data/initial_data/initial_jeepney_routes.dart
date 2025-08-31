7e6a3add9b712e5850f8e5dc042ae3f3e4cc1283
Alexia John D. Pamintuan <rose.pamintuan123@gmail.com>
2025-08-31 21:38:08 +0800

    feat: added new latlong yellow

--- lib/features/routes_list/data/initial_data/initial_jeepney_routes.dart
+++ lib/features/routes_list/data/initial_data/initial_jeepney_routes.dart
@@ -121,6 +121,7 @@ final initialJeepneyRoutes = <JeepneyRoute>[
       LatLng(15.137865230012007, 120.5888958062399), // Plaridel St.
       LatLng(15.138362343742887, 120.58831711964498), // Happy Go Shopping
       LatLng(15.138768836916656, 120.58798989014517), // Puregold Angeles
+      LatLng(15.138789549923345, 120.5878081704844), // San Nicolas Market
     ],
   ),
 ];
